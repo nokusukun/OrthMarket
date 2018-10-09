cff618de5f5391f92512f7755c60d8615dc69180
nokusukun <nokusukun@yahoo.co.jp>
2018-10-10 03:26:51 +0800

    Updated solidity contract

--- OrthMarket.sol
+++ OrthMarket.sol
@@ -1,109 +1,66 @@
 pragma solidity ^0.4.7;
-
 contract OrthMarket {
-    /*
-    OrthMarket.Item
-        Data structure for an item object being stored in the contract
-        ---
-        string **name**
-            - item name, does not require to be unique.
-
-        uint **value**
-            - item value in Orth.
-        
-        string **d**;
-            - item data, could be item metadata or the whole item object encoded 
-                in string.
-
-        string **signature**l
-            - item signature, this is the object unique identifier.
-        
-        address **owner**;
-            - DEPRECATED, item ownership is now being tracked through 
-                (OrthMarket.Ownership).owner
-
-    */
+    
+    event Log (string txid, string value);
+    
     struct Item {
-        string name;
-        uint value;
-        string d;
-        string signature;
-        address owner;
+        string name;        // Item name
+        uint value;         // Item Cost1
+        string d;           // Hash for IPFS data
+        string signature;   // Item Hash
+        address owner;      // Owner
     }
     
-    /*
-    OrthMarket.ItemState
-        Data structure for tracking item state in the contract
-        ---
-        address owner
-            - Owner of the item/signature. 
-                This is modified through OrthMarket.buyItem 
-                    or OrthMarket.registerItem
-        bool inMarket
-            - Sets the state wether the item is in the marketplace or not. 
-                Prevents multiple submission of the same item in the marketplace.
-
-    */
     struct ItemState {
-        address owner;
+        address owner;      
         bool inMarket;
     }
     
-
     address administrator;
     Item[] Warehouse;
     mapping(address => uint) Balance;
-    mapping(string => ItemState) Ownership;
-    uint lastIndex;
-    bool payout;
     
+    mapping(string => ItemState) Ownership;
+    // Item.signature => ItemState
+    
+    uint lastIndex;
+    
+    function OrthMarket() public {
+        administrator = msg.sender;
+        lastIndex = 0;
+    }
+
     modifier inlimits() {
-        require(msg.value >= 1 finney, "Sent value is below minimum value.");
-        require(msg.value % 1 finney == 0, "Sent value is not divisible by 1 finney.");
+        require(msg.value >= 1 finney);
+        require(msg.value % 1 finney == 0);
         _;
+    }
+    
+    function getOwner() public view returns(address owner){
+        owner = administrator;
     }
     
     function buyOrth() public payable inlimits{
-        Balance[msg.sender] += msg.value / 1000000000;
+        Balance[msg.sender] += msg.value / 1 finney;
     }
     
-
     function payoutOrth(uint ammount) public payable {
-        require(Balance[msg.sender] > ammount, "Not enough orth conduct payout.");
-        require(payout == true, "The contract owner disabled orth payout.")
-        msg.sender.transfer(ammount * 1000000000);
+        msg.sender.transfer(ammount * 1 finney);
         Balance[msg.sender] -= ammount;
     }
     
-    constructor() public {
-        administrator = msg.sender;
-        lastIndex = 0;
-        payout = true;
-    }
-
-    modifier adminOnly {
-        require(msg.sender == administrator, "Function for contract owner only.");
-        _;
-    }
-    
-    function togglePayout(bool b) public adminOnly{
-        payout = b;
-    }
-
-    function sendCredits(address recipient, uint value) public returns(bool succesful, string data){
+    function sendCredits(string txid, address recipient, uint value) public {
         if(msg.sender == administrator){
             Balance[recipient] += value;
-            succesful = true;
-            data = "Sucessfuly sent ammount";
+            
+            emit Log(txid, "OK:Sucessfuly sent ammount.");
         }else{
             if(Balance[msg.sender] < value){
-                succesful = false;
-                data = "Not enough balance";
+                emit Log(txid, "ERROR:Not enough balance.");
             }else{
                 Balance[recipient] += value;
                 Balance[msg.sender] -= value;
-                succesful = true;
-                data = "Sucessfuly sent ammount";
+                emit Log(txid, "OK:Sucessfuly sent ammount.");
             }
         }
     }
@@ -115,12 +72,21 @@ contract OrthMarket {
         
     }
     
-    function myBalance() public view returns (address dest, uint value){
+    function myBalance() view public returns (address dest, uint value){
         dest = msg.sender;
         value = Balance[msg.sender];
     }
     
-    function getItem(uint index) public view returns (
+    function getBalance(address user) view public returns (address dest, uint value){
+        dest = user;
+        value = Balance[user];
+    }
+    
+    function countWarehouse() view public returns (uint count){
+        count = Warehouse.length;
+    }
+    
+    function getItem(uint index) view public returns (
         string _name, 
         uint _value,
         string _d,
@@ -128,7 +94,9 @@ contract OrthMarket {
         address _owner
         )
     {
+        require(index != lastIndex, "This index is empty.");
         Item storage item = Warehouse[index];
+        require(Ownership[item.signature].inMarket, "Item is not for sale.");
         _name = item.name;
         _value = item.value;
         _d = item.d;
@@ -148,7 +116,6 @@ contract OrthMarket {
             returnMessage = "Item signature not registered to message sender.";
             return;
         }
-        bool x = !true;
         if(Ownership[_signature].inMarket){
             succesful = false;
             returnMessage = "Item already in the market.";
@@ -172,6 +139,25 @@ contract OrthMarket {
         Ownership[_signature].inMarket = true;
     }
     
+    
+    function removeItem(uint index) public returns(bool succesful){
+        require(Warehouse[index].owner == msg.sender, "You don't own this.");
+        Item storage item = Warehouse[index];
+        Ownership[item.signature].inMarket = false;
+        Warehouse[index] = Warehouse[lastIndex-1];
+        lastIndex -= 1;
+        succesful = true;
+    }
+    
+    
+    function getSignatureOwner(string signature) public view returns
+    (address user, bool inMarket){
+        user = Ownership[signature].owner;
+        inMarket = Ownership[signature].inMarket;
+    }
+    
+    // Implemented to compensate for the inmmaturity of the
+    // web3.py library.
     function buyItem(uint index) public returns(
         bool succesful,
         string data,
@@ -194,7 +180,6 @@ contract OrthMarket {
             _signature = item.signature;
             Warehouse[index] = Warehouse[lastIndex-1];
             lastIndex -= 1;
-            
             Ownership[_signature].owner = msg.sender;
             Ownership[_signature].inMarket = false;
             Balance[item.owner] += _value;
